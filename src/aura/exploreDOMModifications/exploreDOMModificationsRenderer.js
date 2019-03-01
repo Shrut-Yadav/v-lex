e7f6efeeb8f925b21cc1bb19dee6bb3bb1b14b42
Salesforce Casts <teja@salesforcecasts.com>
2019-03-01 08:03:08 +0530

    Component Events

--- src/aura/exploreDOMModifications/exploreDOMModificationsRenderer.js
+++ src/aura/exploreDOMModifications/exploreDOMModificationsRenderer.js
@@ -1,14 +1,10 @@
 ({
     render : function(cmp, helper) {
-        var ret = this.superRender();
+        //var ret = this.superRender();
         // do custom rendering here
         
-        console.log(ret);
-        //ret[1].style.border = '2px solid green';
-        ret[1].onclick = function(){alert('Hello');};
-        //ret[1].onclick = function(){cmp.set( 'v.greeting', 'Updated with render')};
-        //console.log(ret[1].baseURI);
-        return ret;
+        //console.log(ret);
+        
     },
      
 })
